cb814b09f9303295f8f96e00ea710ddd9a584406
Hami <980797900@qq.com>
2020-01-08 16:24:27 +0800

    updated by Hami

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -16,7 +16,115 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12">
-          <h1>gallery</h1>
+          <h3>Faculty&Students</h3>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_1.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_2.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_3.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_4.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_5.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_6.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_7.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/group_8.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/student_1.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/student_2.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/student_3.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/student_4.jpg" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row padding-top50">
+        <div class="col-xs-12">
+          <h3>Fun Moments</h3>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_1.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_2.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_3.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_4.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_5.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_6.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_7.jpg" alt />
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3 text-center">
+          <div class="imgWrap">
+            <img class="galleryImg pointer-hand" src="../assets/img/together/fun_8.jpg" alt />
+          </div>
         </div>
       </div>
     </div>
@@ -34,5 +142,20 @@ export default {};
 
 .titleBtn {
   background-image: linear-gradient(90deg, #0092b6, #b61588);
+}
+
+.imgWrap {
+  margin-top: 10px;
+}
+
+.galleryImg {
+  width: 250px;
+  border-radius: 5%;
+}
+
+@media (max-width: 992px) {
+  .galleryImg {
+    width: 150px;
+  }
 }
 </style>
