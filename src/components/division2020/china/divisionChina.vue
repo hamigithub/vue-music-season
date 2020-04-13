19a2627812032a7463a221116d79eb98844c1628
Hami <980797900@qq.com>
2020-04-13 14:58:49 +0800

    updated

--- src/components/division2020/china/divisionChina.vue
+++ src/components/division2020/china/divisionChina.vue
@@ -23,7 +23,19 @@
               <div class="text-center">
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                   <input type="hidden" name="cmd" value="_s-xclick" />
-                  <input type="hidden" name="hosted_button_id" value="CL448JFGF8SKJ" />
+                  <input type="hidden" name="hosted_button_id" value="LXVMMJ5HA8X5L" />
+                  <table>
+                    <tr>
+                      <td>
+                        <input type="hidden" name="on0" value="Participant name(s):" />Participant name(s):
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input type="text" name="os0" maxlength="200" />
+                      </td>
+                    </tr>
+                  </table>
                   <input
                     type="image"
                     src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif"
@@ -31,13 +43,7 @@
                     name="submit"
                     alt="PayPal - The safer, easier way to pay online!"
                   />
-                  <img
-                    alt
-                    border="0"
-                    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                    width="1"
-                    height="1"
-                  />
+                  <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                 </form>
               </div>
               <p class="padding-20 text-center">{{$t('applyFee.ps')}}</p>
